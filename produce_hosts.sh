d75598f88d0e69ae493f49c138231de67e4c7ab1
penghai <penghai_use@foxmail.com>
2019-12-21 21:43:30 +0800

    openstack

---
+++ produce_hosts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cidr=192.168.99.0/24
+startip=192.168.99.221
+endip=192.168.99.223
+user=root
+password=centos
+subnet=`ipcalc -s -n $cidr|awk -F = '{print $2}'|awk -F 0 '{print $1}'`
+start=`echo $startip|awk -F . '{print $NF}'`
+end=`echo $endip|awk -F . '{print $NF}'`
+
+for ip in $(seq $start $end) 
+do
+    echo "$subnet$ip ansible_ssh_host=$subnet$ip ansible_ssh_user=$user ansible_ssh_pass=$password"
+done
